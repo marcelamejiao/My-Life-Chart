f79fc3e33ece7250e958769624906fa0286902ea
marcelamejiao <mejiao24@hotmail.com>
2024-02-02 17:27:09 +1100

    implement a DTO for creating activities

---
+++ server/src/main/java/xyz/marcelamejia/myLifeChartAPI/activity/ActivityCreateDTO.java
@@ -0,0 +1,45 @@
+package xyz.marcelamejia.myLifeChartAPI.activity;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.text.ParseException;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+public class ActivityCreateDTO {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String category;
+
+    @NotNull
+    private OffsetDateTime start;
+
+    @NotNull
+    private OffsetDateTime end;
+
+    @Min(0)
+    @NotNull
+    private Integer distance;
+
+    public ActivityCreateDTO(String name,
+                             String category,
+                             String start,
+                             String end,
+                             Integer distance) throws ParseException {
+        this.name = name;
+        this.category = category;
+        this.start = OffsetDateTime.parse(start);
+        this.end = OffsetDateTime.parse(end);
+        this.distance = distance;
+    }
+
+}
