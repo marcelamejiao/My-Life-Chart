91235ead388de4870220485a1951b759b0a440e5
marcelamejiao <mejiao24@hotmail.com>
2024-02-02 17:27:48 +1100

    clean imports

--- server/src/main/java/xyz/marcelamejia/myLifeChartAPI/activity/ActivityCreateDTO.java
+++ server/src/main/java/xyz/marcelamejia/myLifeChartAPI/activity/ActivityCreateDTO.java
@@ -1,6 +1,5 @@
 package xyz.marcelamejia.myLifeChartAPI.activity;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
