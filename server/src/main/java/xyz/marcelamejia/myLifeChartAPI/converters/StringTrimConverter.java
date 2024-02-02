bfa7765e978ee445a158d2b1ca751f3f6e5d992e
marcelamejiao <mejiao24@hotmail.com>
2024-02-02 15:01:00 +1100

    implement string trim converter file

---
+++ server/src/main/java/xyz/marcelamejia/myLifeChartAPI/converters/StringTrimConverter.java
@@ -0,0 +1,17 @@
+package xyz.marcelamejia.myLifeChartAPI.converters;
+
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+
+public class StringTrimConverter implements Converter<String, String> {
+
+    public String convert(MappingContext<String, String> context) {
+
+        if(context.getSource() == null) {
+            return null;
+        }
+
+        return context.getSource().trim();
+
+    }
+}
