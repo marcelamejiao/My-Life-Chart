40a7560d5629f564c5ea03f463c9142c7b98244a
marcelamejiao <mejiao24@hotmail.com>
2024-02-02 17:29:51 +1100

    create a activity repository

---
+++ server/src/main/java/xyz/marcelamejia/myLifeChartAPI/activity/ActivityRepository.java
@@ -0,0 +1,6 @@
+package xyz.marcelamejia.myLifeChartAPI.activity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+}
