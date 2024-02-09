7b5cbb790cba9901ef7868d38dea2b3cec1f4fef
marcelamejiao <mejiao24@hotmail.com>
2024-02-09 17:49:38 +1100

    update inentation

--- client/src/models/activities.ts
+++ client/src/models/activities.ts
@@ -1,19 +1,19 @@
 type Activity = {
-    id: number;
-    name: string;
-    category: string;
-    start: Date;
-    end: Date;
-    distance: string;
+  id: number;
+  name: string;
+  category: string;
+  start: Date;
+  end: Date;
+  distance: string;
 }
 
 export type ActivityJson = {
-    id: number;
-    name: string;
-    category: string;
-    start: string;
-    end: string;
-    distance: string;
+  id: number;
+  name: string;
+  category: string;
+  start: string;
+  end: string;
+  distance: string;
 }
 
 export default Activity;
