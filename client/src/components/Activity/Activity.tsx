2a6f5929b4394596656a83651f12f4d90d2d1885
marcelamejiao <mejiao24@hotmail.com>
2024-02-14 16:17:12 +1100

    update TODOS

--- client/src/components/Activity/Activity.tsx
+++ client/src/components/Activity/Activity.tsx
@@ -6,23 +6,18 @@ type Props = {
 
 const Activity = ({ activity }: Props) => {
 
-  // Show the time 
+  // TODO Show the activity date
   const startTime = activity.start;
   console.log(startTime);
 
-  // Calculate duration of the activity 
+  // TODO Calculate duration of the activity 
 
   return (
     <>
       <h1>{activity.name}</h1>
       <h2>{activity.category}</h2>
       <p>{activity.distance}</p>
-      {/* <p>{activity.start}</p>
-      <p>{activity.end}</p> */}
       <p>Duration:</p>
-
-    
-    
     </>
   )
 }
