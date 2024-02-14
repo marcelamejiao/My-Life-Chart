cb382dfb5a9aa31e13a03c64457d5971c79132dc
marcelamejiao <mejiao24@hotmail.com>
2024-02-14 15:02:56 +1100

    update dashboard with activities section

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,5 @@
+
+import { useNavigate } from "react-router-dom";
 import User from "../../models/users";
 
 type Props = {
@@ -5,10 +7,29 @@ type Props = {
 };
 
 const Dashboard = ({ selectedUser }: Props) => {
+
+  const navigate = useNavigate();
+
+  const navigateToActivities = () => {
+    navigate("/activities")
+  }
+
   return (
-    <div>
-        <h1>Welcome</h1>
-        {selectedUser?.name}
+    <div className="p-4">
+        <h1 className="text-xl py-2">Welcome {selectedUser?.name}</h1>
+        <div className="w-80 p-2 rounded border border-solid border-gray-200 shadow-md shadow-gray-400">
+          <div className="p-3">
+            <h2>Activities</h2>
+            <p>This section allows you to see a list of your activities.</p>
+          </div>
+          <div>
+            <button 
+              onClick={navigateToActivities}
+              className="cursor-pointer rounded-md bg-orange-500 text-white p-2 w-30">
+              My Activities
+            </button>
+          </div>
+        </div>
     </div>
   );
 };
