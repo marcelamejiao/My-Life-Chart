332e7bd0c602de413e1249c0faac2a42aa805246
marcelamejiao <mejiao24@hotmail.com>
2024-02-15 18:37:33 +1100

    update login page styling

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -55,36 +55,44 @@ export default function Login({ setSelectedUser }: Props) {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <select 
-          id="users"
-          {...register("selectedUserId",
-          {required: true})}
-        >
-          <option value="">Select user...</option>
-          {allUsers.map((user) => {
-            return (<option key={user.id} value={user.id}>{user.name}</option>)
-          })}
-        </select>
-        {errors.selectedUserId?.type === "required" && (
-          <p role="alert" className="text-red-500">Please select a user from the list</p>
-        )}
+    <>
+      <div className="flex flex-col justify-center pr-4 pt-2">
+      <h1 className="font-title text-4xl tracking-wide text-center text-sky-800 py-4">My Life Chart</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex justify-center m-2">
+          <select 
+            id="users"
+            {...register("selectedUserId",
+            {required: true})}
+            className="rounded p-2 text-sky-800 shadow-sm shadow-black "
+          >
+            <option value="">Select user...</option>
+            {allUsers.map((user) => {
+              return (<option key={user.id} value={user.id}>{user.name}</option>)
+            })}
+          </select>
+          {errors.selectedUserId?.type === "required" && (
+            <p role="alert" className="text-red-500">Please select a user from the list</p>
+          )}
+        </div>
+        <div className="flex justify-center m-2">
+          <input
+              type="submit"
+              value="Login"
+              className="cursor-pointer rounded-md  shadow-sm shadow-black  bg-amber-500 hover:bg-lime-500 text-white p-1 w-20"
+          />
+        </div>
+        <div className="flex justify-center m-2">
+          <p           
+            onClick={navigateToRegister}
+            className="text-center cursor-pointer rounded-md  shadow-sm shadow-black  bg-amber-500 hover:bg-lime-500 text-white p-1 w-20"
+          >Register
+          </p>
+        </div>
+      </form>
       </div>
-      <div>
-        <input
-            type="submit"
-            value="Login"
-            className="cursor-pointer rounded-md  bg-amber-500 hover:bg-lime-500 text-white p-1 w-20"
-        />
-			</div>
-      <div>
-        <p           
-          onClick={navigateToRegister}
-          className="cursor-pointer underline-offset-1"
-        >Register
-        </p>
-      </div>
-    </form>
+    <div className="bg-cover bg-[url('./assets/login-wallpaper.jpg')] w-full "></div>
+    </>
+
   )
 }
