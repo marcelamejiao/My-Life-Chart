3ae2c729743afd88d7ccc41cbb6b98014e28f47a
marcelamejiao <mejiao24@hotmail.com>
2024-02-15 17:08:55 +1100

    update styling

--- client/src/components/SideBar/SideBar.tsx
+++ client/src/components/SideBar/SideBar.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import User from "../../models/users";
 import HoverableNavLink from "../HoverableNavLink/HoverableNavLink";
 
@@ -6,19 +7,25 @@ type Props = {
 }
 
 const SideBar = ({ selectedUser }:Props) => {
+  const navigate = useNavigate();
 
   // For TypeScript requirements
   if(!selectedUser) {
     return ;
   }
 
+  const navigateToDashboard = () => {
+    navigate('/dashboard');
+  }
+
   return (    
   <div className="h-full w-48 z-10 top-0 left-0 text-white bg-sky-800 overflow-x-hidden pt-2">
-    <p className="font-title text-2xl tracking-wide p-2">My Life Chart</p>
-    <h1 className="p-2 text-xl tracking-wide bg-white text-lime-500 mx-2 my-3 rounded text-center font-bold shadow-m shadow-black border border-gray-400 border-dashed" >{selectedUser.name}</h1>
+    <p className="font-title text-2xl tracking-wide p-2 hover:animate-bounce">My Life Chart</p>
+    <h1 onClick={navigateToDashboard}
+      className="p-1 text-lg tracking-wide hover:scale-105 bg-slate-50 hover:bg-white text-gray-500 font-semibold mx-2 my-3 rounded text-center shadow-sm shadow-black cursor-pointer" >{selectedUser.name}</h1>
 
-    <ul className="flex justify-end pr-2">
-      <li>
+    <ul className="flex justify-end pr-2 hover:translate-x-0.5 text-sky-200 v bg-sky-700 w-48 py-2 shadow-sm shadow-black">
+      <li className="hover:text-white">
         <HoverableNavLink
             pathName="/form"
             linkText="Create Activity">
