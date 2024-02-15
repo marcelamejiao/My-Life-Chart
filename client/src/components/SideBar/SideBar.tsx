4b83a823ec139a33940bbf7c87f0f0f46435ef62
marcelamejiao <mejiao24@hotmail.com>
2024-02-15 16:29:21 +1100

    update sidebar styling

--- client/src/components/SideBar/SideBar.tsx
+++ client/src/components/SideBar/SideBar.tsx
@@ -13,11 +13,11 @@ const SideBar = ({ selectedUser }:Props) => {
   }
 
   return (    
-  <div className="h-full w-48 z-10 top-0 left-0 bg-red-50 overflow-x-hidden pt-2">
-    <p className="font-title text-2xl">My Life Chart</p>
-    <h1>{selectedUser.name}</h1>
+  <div className="h-full w-48 z-10 top-0 left-0 text-white bg-sky-800 overflow-x-hidden pt-2">
+    <p className="font-title text-2xl tracking-wide p-2">My Life Chart</p>
+    <h1 className="p-2 text-xl tracking-wide bg-white text-lime-500 mx-2 my-3 rounded text-center font-bold shadow-m shadow-black border border-gray-400 border-dashed" >{selectedUser.name}</h1>
 
-    <ul>
+    <ul className="flex justify-end pr-2">
       <li>
         <HoverableNavLink
             pathName="/form"
