5ac678847e59c06d4d11d2c7d2a9aaf939e6b251
marcelamejiao <mejiao24@hotmail.com>
2024-02-15 18:02:17 +1100

    update sudebar styling

--- client/src/components/SideBar/SideBar.tsx
+++ client/src/components/SideBar/SideBar.tsx
@@ -24,11 +24,17 @@ const SideBar = ({ selectedUser }:Props) => {
     <h1 onClick={navigateToDashboard}
       className="p-1 text-lg tracking-wide hover:scale-105 bg-slate-50 hover:bg-white text-gray-500 font-semibold mx-2 my-3 rounded text-center shadow-sm shadow-black cursor-pointer" >{selectedUser.name}</h1>
 
-    <ul className="flex justify-end pr-2 hover:translate-x-0.5 text-sky-200 v bg-sky-700 w-48 py-2 shadow-sm shadow-black">
-      <li className="hover:text-white">
+    <ul className="flex flex-col items-end gap-2 py-4 pr-2 text-sky-200 bg-sky-700 w-48 shadow-sm shadow-black">
+      <li className="hover:text-white hover:translate-x-0.5">
         <HoverableNavLink
             pathName="/form"
             linkText="Create Activity">
+        </HoverableNavLink>
+      </li>
+      <li className="hover:text-white hover:translate-x-0.5">
+        <HoverableNavLink
+            pathName="/activities"
+            linkText="My Activities">
         </HoverableNavLink>
       </li>
     </ul>
