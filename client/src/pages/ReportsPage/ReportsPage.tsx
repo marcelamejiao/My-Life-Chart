4a8fc977cc4cfd00b1589644f3615493dfb21b68
marcelamejiao <mejiao24@hotmail.com>
2024-02-27 19:07:44 +1100

    display bar chart for kilometers ran per week in a semester

--- client/src/pages/ReportsPage/ReportsPage.tsx
+++ client/src/pages/ReportsPage/ReportsPage.tsx
@@ -1,7 +1,79 @@
+import { Chart as ChartJS, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title, ChartOptions } from "chart.js";
+import { useEffect, useState } from "react";
+import { Bar } from 'react-chartjs-2';
+import CategoryWeeklyReport from "../../models/categoryWeeklyReports";
+import { getAllCategoryWeeklyReports } from "../../services/categoryWeeklyReports";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options: ChartOptions = {
+  responsive: true,
+  scales: {
+    x: {
+      title: {
+        color: 'rgb(3 105 161)',
+        display: true,
+        text: 'Week'
+      }
+    },
+    y: {
+      title: {
+        color: 'rgb(3 105 161)',
+        display: true,
+        text: 'Kilometers'
+      }
+    }
+  },
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Kilometers ran per week in a Semester',
+      color: 'rgb(3 105 161)'
+    },
+  },
+};
 
 const ReportsPage = () => {
+  const [reports, setReports] = useState<CategoryWeeklyReport[]>([]);
+
+  useEffect(() => {
+    getAllCategoryWeeklyReports().then((reportData: CategoryWeeklyReport[]) => {
+      setReports(reportData);
+    })
+  }, []);
+
   return (
-    <></>
+    <>
+      <Bar
+        options={options}
+        data={
+          {
+            labels: reports.map((report: CategoryWeeklyReport) => {
+              return report.week;
+            }),
+            datasets: [
+              {
+                label: 'Km',
+                data: reports.map((report: CategoryWeeklyReport) => {
+                  return report.km;
+                }),
+                backgroundColor: 'rgba(231, 210, 251)'
+              }
+            ]
+          }
+        }
+      />
+    </>
   )
 }
 
