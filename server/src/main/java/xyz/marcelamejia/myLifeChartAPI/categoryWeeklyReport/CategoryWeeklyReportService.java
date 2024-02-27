1b6e7762808c2baec4d481d2f762b78ea9222f46
marcelamejiao <mejiao24@hotmail.com>
2024-02-27 16:05:25 +1100

    implement foor loop to return a list of weekly distance report

--- server/src/main/java/xyz/marcelamejia/myLifeChartAPI/categoryWeeklyReport/CategoryWeeklyReportService.java
+++ server/src/main/java/xyz/marcelamejia/myLifeChartAPI/categoryWeeklyReport/CategoryWeeklyReportService.java
@@ -14,39 +14,43 @@ import java.util.List;
 public class CategoryWeeklyReportService {
     final String ISO8601Format = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
 
-
     @Autowired
     private ActivityRepository activityRepository;
 
     public List<CategoryWeeklyReport> get() {
-        Calendar sixMonthsAgo = Calendar.getInstance();
-        sixMonthsAgo.add(Calendar.MONTH, -6);
-
-        SimpleDateFormat iso8601DateFormat = new SimpleDateFormat(ISO8601Format);
-
-        // Find the beginning of the week
-        Calendar beginningOfWeek = (Calendar) sixMonthsAgo.clone();
-        beginningOfWeek.add(Calendar.DAY_OF_WEEK,
-                beginningOfWeek.getFirstDayOfWeek() - beginningOfWeek.get(Calendar.DAY_OF_WEEK));
-
-        // Find the end of the week
-        Calendar endOfWeek = (Calendar) beginningOfWeek.clone();
-        endOfWeek.add(Calendar.DAY_OF_WEEK, 6);
-
-        String formattedBeginningOfTheWeek = iso8601DateFormat.format(beginningOfWeek.getTime());
-        String formattedEndOfTheWeek = iso8601DateFormat.format(endOfWeek.getTime());
-
-        OffsetDateTime beginningOfTheWeekOffsetDateTime = OffsetDateTime.parse(formattedBeginningOfTheWeek);
-        OffsetDateTime endOfTheWeekOffsetDateTime = OffsetDateTime.parse(formattedEndOfTheWeek);
-
-        ISum sumOfDistance = this.activityRepository.sumDistanceBetweenDates(beginningOfTheWeekOffsetDateTime, endOfTheWeekOffsetDateTime);
-        int weeklyDistance = sumOfDistance.getSum();
-
-        System.out.print(sumOfDistance.getSum());
 
         List<CategoryWeeklyReport> report = new ArrayList<>();
-        report.add(new CategoryWeeklyReport(1, weeklyDistance));
 
+        for (int i = 0; i < 168; i+=7) {
+            Calendar sixMonthsAgo = Calendar.getInstance();
+            sixMonthsAgo.add(Calendar.MONTH, -6);
+
+            SimpleDateFormat iso8601DateFormat = new SimpleDateFormat(ISO8601Format);
+
+            // Find the beginning of the week
+            Calendar beginningOfWeek = (Calendar) sixMonthsAgo.clone();
+            beginningOfWeek.add(Calendar.DAY_OF_WEEK,
+                    beginningOfWeek.getFirstDayOfWeek() - beginningOfWeek.get(Calendar.DAY_OF_WEEK) + i);
+
+            // Find the end of the week
+            Calendar endOfWeek = (Calendar) beginningOfWeek.clone();
+            endOfWeek.add(Calendar.DAY_OF_WEEK, 6);
+
+            String formattedBeginningOfTheWeek = iso8601DateFormat.format(beginningOfWeek.getTime());
+            String formattedEndOfTheWeek = iso8601DateFormat.format(endOfWeek.getTime());
+
+            OffsetDateTime beginningOfTheWeekOffsetDateTime = OffsetDateTime.parse(formattedBeginningOfTheWeek);
+            OffsetDateTime endOfTheWeekOffsetDateTime = OffsetDateTime.parse(formattedEndOfTheWeek);
+
+            ISum sumOfDistance = this.activityRepository.sumDistanceBetweenDates(beginningOfTheWeekOffsetDateTime, endOfTheWeekOffsetDateTime);
+            int weeklyDistance = sumOfDistance.getSum();
+
+            System.out.print(sumOfDistance.getSum());
+
+            int week = (i / 7) + 1;
+
+            report.add(new CategoryWeeklyReport(week, weeklyDistance));
+        }
         return report;
     }
 }
